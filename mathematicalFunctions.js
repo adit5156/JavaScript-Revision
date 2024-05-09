541025c959267e3d71e8c1e6a9ced4bcf8221334
adit5156 <143902868+adit5156@users.noreply.github.com>
2024-05-09 12:12:18 +0530

    updated

---
+++ mathematicalFunctions.js
@@ -0,0 +1,21 @@
+// console.log(Math.abs(-7));
+// console.log(Math.abs(8));
+
+// console.log(Math.max(5,1));
+// console.log(Math.min(-5,1));
+// console.log(Math.max(4,8,19,50));
+
+// console.log(Math.pow(2,4));
+// console.log(Math.sqrt(529));
+
+// console.log(Math.cbrt(27));
+// console.log(Math.cbrt(8));
+// console.log(Math.cbrt(512));
+
+// console.log(Math.floor(5.9));
+// console.log(Math.floor(12.7));
+// console.log(Math.floor(-3.5));
+
+// console.log(Math.ceil(5.1));
+
+console.log(Math.random());
---
+++ ques1.js
@@ -0,0 +1,29 @@
+// Write a code to generate a random number between 0 to 9 (included)!
+
+
+// let ran = Math.floor(Math.random()*10);
+// console.log(ran);
+
+// HOMEWORK
+// 1) Write a code to generate a random integer between 0 and 10 (included)
+// Answer
+// console.log(Math.floor(Math.random()*11));
+
+// 2) Wirte a code to generate a random integer between -10 and 10 (included)
+// Answer
+// console.log(Math.floor(-10 + Math.random()*(11-(-10))));
+
+
+// if(Math.random()<0.5) {
+//     console.log("Hello Mr.Aditya");
+// }
+// else {
+//     console.log("Byee Mr.Aditya");
+// }
+
+// 3) Generate a random number between 10 and 20
+// let x = 10;
+// let y = 21;
+// let ran = Math.floor(x + Math.random()*(y-x));
+// console.log(ran);
+
